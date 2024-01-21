4079d8fdc5c6bc572eb11ba1e6c0cb14dc353eff
SelvedinFrontEnd <121358456+SelvedinFrontEnd@users.noreply.github.com>
2024-01-21 23:18:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -11,9 +11,19 @@ let upperCart = document.querySelector(".upper-cart");
 let fullCart;
 let cartNumber = document.querySelector(".cart-number");
 
+/*function handleCartMenuVisibility(){
+    if(window.innerWidth > 800){
+    cartMenu.style.visibility = "hidden"
+    emptyCart.style.visibility = "hidden"
+}
+}
+
+handleCartMenuVisibility()
+
+window.addEventListener("resize", handleCartMenuVisibility)*/
 
 cart.addEventListener("click", () =>{
-    if(cartMenu.style.visibility === "visible"){
+         if(cartMenu.style.visibility === "visible"){
         cartMenu.style.visibility = "hidden"
         emptyCart.style.visibility = "hidden";
     }else {
@@ -21,7 +31,7 @@ cart.addEventListener("click", () =>{
         if(getComputedStyle(upperCart).display === "none"){
             emptyCart.style.visibility = "visible"; 
         }
-    }
+    } 
 })
 
 plus.addEventListener("click", () => {
@@ -66,8 +76,9 @@ function cartAdd(){
                                 <div class="gray">Fall Limited Edition Sneakers</div>
                                 <div class="gray">$125.00 x ${count} <span class="dark"> $${total.toFixed(2)}</span></div>
                                 </div>
-                                <svg onClick="deleteItem()" class"remove" width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a"/></svg>
-                                </div>
+                                <svg onClick="deleteItem()" class="remove" width="25" height="25" xmlns="http://www.w3.org/2000/svg">
+                                <path fill="#C3CAD9" fill-rule="nonzero" d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z"/>
+                            </svg>                                </div>
                                 <button class="checkout">Checkout</button>
                                 ` 
     }
